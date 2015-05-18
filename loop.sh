10b0d2fa2473d86a16e18c4b97469d446edd1349
sjx <shijiaxin.cn@gmail.com>
2015-05-18 10:33:05 +0800

    loop in array

--- loop.sh
+++ loop.sh
@@ -11,8 +11,23 @@ for((i =$(($total_i -1));i>=0;i-- ));do
         done
 done
 
+#loop in a simple list
 echo "running x-in-a-list"
 for x in 1 4 7 10;
 do
     echo $x
 done 
+
+#loop in complex array
+echo "running loop in complex array"
+#  ${arr[*]}         # All of the items in the array
+#  ${!arr[*]}        # All of the indexes in the array
+#  ${#arr[*]}        # Number of items in the array
+#  ${#arr[0]}        # Length of item zero
+A=(1 2 3) #no space before or after =
+B=(Jan Feb Mar)
+for i in ${!A[*]};
+do
+  echo ${B[$i]} ${A[$i]}
+done
+
