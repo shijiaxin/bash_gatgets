a561002db714f679d5beae06a71087db101d794d
sjx <shijiaxin.cn@gmail.com>
2015-05-18 10:02:27 +0800

    loop

--- loop.sh
+++ loop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-total_j=6
-total_i=4
+#double loop
+echo "running double-for-loop"
+total_j=5
+total_i=3
 for((i =$(($total_i -1));i>=0;i-- ));do
         for((j =$(($total_j -1));j>=0;j-- ));do
         curr=$(($i * $total_j + $j))
@@ -9,4 +11,8 @@ for((i =$(($total_i -1));i>=0;i-- ));do
         done
 done
 
-
+echo "running x-in-a-list"
+for x in 1 4 7 10;
+do
+    echo $x
+done 
---
+++ user_related.sh
@@ -0,0 +1,7 @@
+
+
+#添加新帐号
+sudo adduser sjx
+
+#添加到sudo list里面
+sudo usermod -a -G sudo sjx
